f5d628f8ad365e33c88182b848b84dc028bf064e
zliaa Elyes <zliaaelyes@gmail.com>
2019-12-02 01:08:32 +0100

    terminer chat room avec singlR

--- ADVYTEAM.Presentation/signalr/hubs/ChatHub.cs
+++ ADVYTEAM.Presentation/signalr/hubs/ChatHub.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ADVYTEAM.Data;
+using ADVYTEAM.Services;
 using Microsoft.AspNet.SignalR;
 
 namespace LiveChat.signalr.hubs
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        IUtilisateurService serviceUtilisateur;
+        public ChatHub()
         {
-            Clients.All.addNewMessageToPage(name, message);
+            serviceUtilisateur = new UtilisateurService();
         }
+        public void Send(string name, string message,string id)
+        {
+            utilisateur user = serviceUtilisateur.GetById(Int32.Parse(id));
+            string  dt = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
+            Clients.All.addNewMessageToPage(name, message,dt, user.image);
+        }
+
     }
 }
